8f98120a4b9d2ae4d1d09fd45df5232f466e31ce
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-09-27 18:34:10 +0300

    Додано пошук крайніх значень піку.

--- module/length_peak_sequence.py
+++ module/length_peak_sequence.py
@@ -9,4 +9,17 @@ def length_peak_sequence(array: list[int]) -> int:
             i += 1
             continue
 
+        left_index = i - 2
+        while left_index >= 0 and array[left_index] < array[left_index + 1]:
+            left_index -= 1
+
+        right_index = i + 2
+        while right_index < len(array) and array[right_index] < array[right_index - 1]:
+            right_index += 1
+
+        current_peak_length = right_index - left_index - 1
+        longest_peak_length = max(longest_peak_length, current_peak_length)
+
+        i = right_index
+        
     return longest_peak_length
