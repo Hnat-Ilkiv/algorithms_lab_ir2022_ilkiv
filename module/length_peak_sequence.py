61bee086f072690f9b30f3659ae0da4d65e2ea98
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-09-27 18:33:20 +0300

    Коректно відпрацьовує 3 тест.

--- module/length_peak_sequence.py
+++ module/length_peak_sequence.py
@@ -1,4 +1,12 @@
 def length_peak_sequence(array: list[int]) -> int:
     longest_peak_length = 0
-    
+    i = 1
+
+    while i < len(array) - 1:
+        is_peak = array[i - 1] < array[i] > array[i + 1]
+
+        if not is_peak:
+            i += 1
+            continue
+
     return longest_peak_length
