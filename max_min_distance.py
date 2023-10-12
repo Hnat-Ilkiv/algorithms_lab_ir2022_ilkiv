4328f3a38344f6d0c17125db649e911dd6607033
Hnat Ilkiv <119300217+Hnat-Ilkiv@users.noreply.github.com>
2023-10-12 16:07:42 +0300

    finish work with max_min_distance

--- max_min_distance.py
+++ max_min_distance.py
@@ -6,27 +6,33 @@ def max_min_distance(N: int, C: int, free_sections: list[int]) -> int:
     high = free_sections[-1] - free_sections[0]
     
     result = 0
+    result_section = []
     
     while low <= high:
         mid = (low + high) // 2
+        busy_section = []
         
-        if can_place_cows(mid, N, C, free_sections):
+        if can_place_cows(mid, N, C, free_sections, busy_section):
             result = mid
+            result_section = busy_section
             low = mid + 1
         else:
             high = mid - 1
 
+    print(f"{result} -> {result_section}")
     return result
 
-def can_place_cows(mid: int, N: int, C: int, free_sections: list[int]) -> bool:
+def can_place_cows(mid: int, N: int, C: int, free_sections: list[int], busy_section: list[int]) -> bool:
     cows_placed = 1
     
     prev_stall = free_sections[0]
+    busy_section.append(free_sections[0])
     
     for i in range(1, N):
         if free_sections[i] - prev_stall >= mid:
             cows_placed += 1
             prev_stall = free_sections[i]
+            busy_section.append(prev_stall)
             if cows_placed == C:
                 return True
     return False
@@ -56,8 +62,13 @@ def merge(left: list[int], right: list[int]) -> list[int]:
     return result
 
 if __name__ == "__main__":
-    N = 5
-    C = 3
-    free_sections = [1, 2, 8, 4, 9]
+    # N = 5
+    # C = 3
+    # free_sections = [1, 2, 8, 4, 9]
+    # result = max_min_distance(N, C, free_sections)
+    # print(result)
+
+    N = 10
+    C = 5
+    free_sections = [90, 60, 40, 30, 10, 5, 4, 3, 2, 1]
     result = max_min_distance(N, C, free_sections)
-    print(result)
