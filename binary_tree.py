318e7f99353d32e0197c3a74cd31a0fa00eb7368
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-10-25 12:46:02 +0300

    add str and repert for Binary Tree

--- binary_tree.py
+++ binary_tree.py
@@ -3,3 +3,9 @@ class BinaryTree:
         self.value = value
         self.left = left
         self.right = right
+
+    def __repr__(self):
+        return f"value = {self.value};"
+
+    def __str__(self):
+        return f"Value: {self.value}"
