d1ed284c848118f95b3dd529ddc104c870795026
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-10-26 11:48:23 +0300

    release version

--- is_tree_balanced.py
+++ is_tree_balanced.py
@@ -1,35 +1,35 @@
-class BinaryTree:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+from binary_tree import BinaryTree
+from display_ascii_tree import display_ascii_tree_horizontally
+
+def check_balanced(node):
+    if node is None:
+        return 0
+
+    left_height = check_balanced(node.left)
+    # print(node,"\tleft", left_height)
+    if left_height == -1:
+        return -1
+
+    right_height = check_balanced(node.right)
+    # print(node,"\tright", right_height)
+    if right_height == -1:
+        return -1
+    # print(node, "\t\tbal", left_height - right_height)
+    if abs(left_height - right_height) > 1:
+        return -1
+    # print(node,"\t\t\treturn", max(left_height, right_height) + 1)
+    return max(left_height, right_height) + 1
 
 def is_tree_balanced(node: BinaryTree) -> bool:
-    def check_balanced(node):
-        if node is None:
-            return 0
-
-        left_height = check_balanced(node.left)
-        if left_height == -1:
-            return -1
-
-        right_height = check_balanced(node.right)
-        if right_height == -1:
-            return -1
-
-        if abs(left_height - right_height) > 1:
-            return -1
-
-        return max(left_height, right_height) + 1
-
     return check_balanced(node) != -1
 
 
-# Приклад використання
 root = BinaryTree(3)
 root.left = BinaryTree(9)
 root.right = BinaryTree(20)
 root.right.left = BinaryTree(15)
 root.right.right = BinaryTree(7)
 
-print(is_tree_balanced(root))  # Повинно вивести True
+display_ascii_tree_horizontally(root)
+
+print(is_tree_balanced(root))
