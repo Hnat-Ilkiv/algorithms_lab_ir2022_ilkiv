377635c8bd91b52f51809b5fa7ff7444c9074325
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-11-01 20:52:23 +0200

    flood fill without recursion

--- flood_fill.py
+++ flood_fill.py
@@ -8,21 +8,16 @@ def flood_fill(matrix_height, matrix_width, matrix, start, target_color, replace
         print (f"Height && Width Error")
         return matrix
 
-    def is_valid(x, y):
-        return 0 <= x < height and 0 <= y < width
+    stack = [start]
 
-    def fill(x, y):
-        if is_valid(x, y) and matrix[x][y] == target_color:
+    while stack:
+        x, y = stack.pop()
+        if 0 <= x < height and 0 <= y < width and matrix[x][y] == target_color:
             matrix[x][y] = replace_color
-            fill(x + 1, y)
-            fill(x - 1, y)
-            fill(x, y + 1)
-            fill(x, y - 1)
-
-    start_x, start_y = start
-    print(start_x, start_y)
-    print(matrix[start_x][start_y])
-    fill(start_x, start_y)
+            stack.append((x + 1, y))
+            stack.append((x - 1, y))
+            stack.append((x, y + 1))
+            stack.append((x, y - 1))
 
 def read_input(file_path):
     with open(file_path, 'r') as f:
@@ -31,7 +26,6 @@ def read_input(file_path):
         start = tuple(map(int, lines[1].strip().split(',')))
         target_color = lines[2].strip().strip('‘’')
         matrix = [list(row.strip().strip('[\'\'],').split('\', \'')) for row in lines[3:]]
-
         return (height, width, start, target_color, matrix)
 
 def write_output(file_path, matrix):
@@ -48,16 +42,16 @@ if __name__ == '__main__':
 
     replace_color = 'C'  # Вказуємо колір для заміни
 
-    print(height, width)
-    print(start)
-    print(target_color, "=>", replace_color)
-    for i in matrix:
-        print(i)
+    # print(height, width)
+    # print(start)
+    # print(target_color, "=>", replace_color)
+    # for i in matrix:
+    #     print(i)
 
     flood_fill(height, width, matrix, start, target_color, replace_color)
 
-    print()
-    for i in matrix:
-        print(i)
+    # print()
+    # for i in matrix:
+    #     print(i)
 
     write_output(output_file, matrix)
