19f7c32695f0824027ba605eb50265aa77cffada
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-11-15 12:03:06 +0200

    assignment through tests

---
+++ test.py
@@ -0,0 +1,37 @@
+from main import find_unreachable
+import unittest
+
+class TestIsConnected(unittest.TestCase):
+
+    def test_connected_case(self):
+        """
+        S_0
+         ↓
+        C_0 → C_1 → C_2
+        !↓
+        S_1
+        """
+        cities = ['City_0', 'City_1', 'City_2']
+        storages = ['Storage_0', 'Storage_1']
+        pipelines = [['City_0', 'City_1'], ['City_1', 'City_2'], ['Storage_0', 'City_0'], ['Storage_1', 'City_0']]
+        result = find_unreachable(cities, storages, pipelines)
+        expected = []
+        self.assertEqual(result, expected)
+
+    def test_disconnected_case(self):
+        """
+        S_0
+         ↓
+        C_0 → C_1 → C_2
+              !↓
+              S_1
+        """
+        cities = ['City_0', 'City_1', 'City_2']
+        storages = ['Storage_0', 'Storage_1']
+        pipelines = [['City_0', 'City_1'], ['City_1', 'City_2'], ['Storage_0', 'City_0'], ['Storage_1', 'City_1']]
+        result = find_unreachable(cities, storages, pipelines)
+        expected = [['Storage_1', ['City_0']]]
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
