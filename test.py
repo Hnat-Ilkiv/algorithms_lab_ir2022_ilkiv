066367b7d9fbedc2632207b31dda72b629b2c30b
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-11-16 10:36:12 +0200

    a task test has been added

--- test.py
+++ test.py
@@ -33,5 +33,18 @@ class TestIsConnected(unittest.TestCase):
         expected = [['Storage_1', ['City_0']]]
         self.assertEqual(result, expected)
 
+    def test_given_situation(self):
+        """
+        C_1 → C_2
+
+        Dolyna → Lviv → Stryy
+        """
+        cities = ['Львів', 'Стрий', 'Долина']
+        storages = ['Сховище_1', 'Сховище_2']
+        pipelines = [['Львів', 'Стрий'], ['Долина', 'Львів'], ['Сховище_1', 'Сховище_2']]
+        result = find_unreachable(cities, storages, pipelines)
+        expected = [['Сховище_1', ['Львів', 'Стрий', 'Долина']], ['Сховище_2', ['Львів', 'Стрий', 'Долина']]]
+        self.assertEqual(result, expected)
+
 if __name__ == '__main__':
     unittest.main()
