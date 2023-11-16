9b9d08cd48f6c0fe0fa53603fa30def5ff544b16
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-11-16 11:30:36 +0200

    the code corresponds to pep8 and added doc strings

--- src/main.py
+++ src/main.py
@@ -1,4 +1,28 @@
-def bfs(city, first_gas_storage, gas_storage, pipelines):
+"""
+Module Description: Gas Transport Algorithm
+
+This module provides functions for analyzing gas transport connectivity
+between gas storage locations and cities using active pipelines.
+
+Author: Hnat Ilkiv
+Date: 2023-11-16
+"""
+
+
+def bfs(
+    first_gas_storage: str, gas_storage: list[str], pipelines: list[str]
+) -> list[str]:
+    """
+    Perform Breadth-First Search (BFS) to find reachable locations from a given gas storage.
+
+    Args:
+        first_gas_storage (str): The starting gas storage.
+        gas_storage (list): List of gas storage locations.
+        pipelines (list): List of active pipelines.
+
+    Returns:
+        list: List of reachable locations.
+    """
     queue = [first_gas_storage]
     visited = []
 
@@ -8,21 +32,37 @@ def bfs(city, first_gas_storage, gas_storage, pipelines):
             visited.append(current_note)
 
             for connection in pipelines:
-                if connection[0] == current_note and connection[1] not in gas_storage:
+                if (
+                    connection[0] == current_note
+                    and connection[1] not in gas_storage
+                ):
                     queue.append(connection[1])
 
     visited.pop(0)
     return visited
 
 
+def find_unreachable(
+    city: list[str], gas_storage: list[str], pipelines: list[str]
+) -> list[str, list[str]]:
+    """
+    Find gas storage locations and the corresponding unreachable cities.
 
+    Args:
+        city (list): List of cities.
+        gas_storage (list): List of gas storage locations.
+        pipelines (list): List of active pipelines.
 
-def find_unreachable(city, gas_storage, pipelines):
+    Returns:
+        list: List of unreachable locations and their corresponding cities.
+    """
     unreachable = []
 
     for storage in gas_storage:
-        available = bfs(city, storage, gas_storage, pipelines)
-        unavailable = [element for element in city if element not in available]
+        available = bfs(storage, gas_storage, pipelines)
+        unavailable = [
+            element for element in city if element not in available
+        ]
 
         if unavailable:
             unreachable.append([storage, unavailable])
